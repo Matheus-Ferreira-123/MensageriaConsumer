e4f31fe4f45c5489772b7a6f0ebbedf9ad374988
Matheus-Ferreira-123 <matheuslopesferreirabh@gmail.com>
2025-06-17 10:16:32 -0300

    Projeto consumer com rabbitmq

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(
+    queue: "fila1",
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null);
+
+var consumer1 = new EventingBasicConsumer(channel);
+consumer1.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($" [x] Recebido: {message}");
+};
+
+channel.BasicConsume(
+    queue: "fila1",
+    autoAck: true,
+    consumer: consumer1);
+
+Console.WriteLine("Aguardando mensagens...");
+Console.ReadLine();
